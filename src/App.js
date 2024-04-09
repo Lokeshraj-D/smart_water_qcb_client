a7a2d8a6e2dc67c6b9d0ceb5ab881774a5354b64
Lokesh <dlr.220044@gmail.com>
2024-04-10 03:20:42 +0530

    Updated firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Signin from './compoents/signin';
 import Signup from './compoents/signup';
 import Header from './compoents/header';
+import Body from './compoents/body';
 
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -11,16 +12,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Router>
+      <Body />
+      {/* <Router>
         <Routes>
           <Route path='/' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/dashboard' element={<Header />} />
-
-
-
         </Routes>
-      </Router>
+      </Router> */}
     </>
   );
 }
